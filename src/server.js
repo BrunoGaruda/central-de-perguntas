3bc009887d8039407016fc86856952be5c0baf7f
BrunoGaruda <bruleorn@gmail.com>
2022-07-31 23:58:44 -0300

    fixing path route on server.js and test on localhost:3000

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const route = require('./route')
+const path = require('path')
 
 //Executando o express
 const server = express()
+
+//view engine será o ejs
+server.set('view engine', 'ejs')
+
+//path: caminho do projeto. join: junta o caminho do projeto com __dirname(src/) com "views"
+server.set('views', path.join(__dirname, 'views'))
 
 server.use(route)
 
