0c51e8f46f4a70afe1f26795ba0736c3fdfad03f
BrunoGaruda <bruleorn@gmail.com>
2022-08-14 15:32:44 -0300

    creating a route for controller

--- src/route.js
+++ src/route.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
 
 const route = express.Router()
 
@@ -8,7 +9,6 @@ route.get('/room', (req, res) => res.render('room'))
 route.get('/create-pass', (req, res) => res.render('create-pass'))
 
 // Formato do formoulário de dentro da modal tem que passar a informação
-// route.get('/room/:room/:question/:action'
-// )
+route.post('/room/:room/:question/:action', QuestionController.index)
 
 module.exports = route
