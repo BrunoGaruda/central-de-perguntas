0d7e7485ab7509575b3ccebb8e54e5e57dcf9573
BrunoGaruda <bruleorn@gmail.com>
2022-08-16 01:34:10 -0300

    create a RoomControler route and fix question and room routes

--- src/route.js
+++ src/route.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
@@ -9,8 +10,10 @@ route.get('/create-pass', (req, res) =>
   res.render('index', { page: 'create-pass' })
 )
 
-route.get('/room', (req, res) => res.render('room'))
+route.get('/room/:room', (req, res) => res.render('room'))
+
 // Formato do formoulário de dentro da modal tem que passar a informação
-route.post('/room/:room/:question/:action', QuestionController.index)
+route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/create-room', RoomController.create)
 
 module.exports = route
