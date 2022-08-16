52e506a7266efa22de03e3d04bab622dd0203859
BrunoGaruda <bruleorn@gmail.com>
2022-08-15 23:57:06 -0300

    refactoring ejs index and create-passq creating new route

--- src/route.js
+++ src/route.js
@@ -4,10 +4,12 @@ const QuestionController = require('./controllers/QuestionController')
 const route = express.Router()
 
 //Requisition, Response
-route.get('/', (req, res) => res.render('index'))
-route.get('/room', (req, res) => res.render('room'))
-route.get('/create-pass', (req, res) => res.render('create-pass'))
+route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
+route.get('/create-pass', (req, res) =>
+  res.render('index', { page: 'create-pass' })
+)
 
+route.get('/room', (req, res) => res.render('room'))
 // Formato do formoulário de dentro da modal tem que passar a informação
 route.post('/room/:room/:question/:action', QuestionController.index)
 
